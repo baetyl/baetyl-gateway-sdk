930ac54b0e14749855fd4e188aa3f0be08e41917
zhaoxin26 <zhaoxin26@baidu.com>
2024-03-13 04:34:00 +0800

    python setup

---
+++ sdk/python/setup.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='gateway_sdk',
+    version='1.0.0',
+    author='UNKNOWN',
+    author_email='UNKNOWN',
+    description='bie gateway python sdk',
+    packages=find_packages(),
+)
